54f6a39d3074586f552e28da01e33229234eedd0
PanArtur <pankovets.artur1@gmail.com>
2026-01-02 21:15:14 +0200

    fixing cors

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const app = express();
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://phonebook-frontend-beige.vercel.app'
+  'https://phonebook-frontend-beige.vercel.app',
+  'https://phonebook-frontend-9be7bninc-arturs-projects-64cbc683.vercel.app'
 ];
 
 // Логування для debug
