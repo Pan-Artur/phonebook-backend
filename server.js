142cb03506984c3a84853a981164491a2c8d4735
PanArtur <pankovets.artur1@gmail.com>
2026-01-02 22:53:40 +0200

    fixing cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV !== "production") {
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3001',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
